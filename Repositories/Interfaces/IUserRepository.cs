3b83efb2687a262f3749861ee837829a0353e84b
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-11 17:17:21 -0300

    feat: interface contracts

---
+++ Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<List<UserModel>> GetAllUsers();
+        Task<UserModel> GetUserById(int id);
+        Task<UserModel> CreateUser(UserModel user);
+        Task<UserModel> UpdateUser(UserModel user, int id);
+        Task<bool> deleteUser(int id);
+    }
+}

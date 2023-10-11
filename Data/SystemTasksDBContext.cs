320dedc4f17f57c23d39c47047ed65b0bf34925c
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-11 17:05:36 -0300

    feat: create dbcontext

---
+++ Data/SystemTasksDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data
+{
+    public class SystemTasksDBContext : DbContext
+    {
+        public SystemTasksDBContext(DbContextOptions<SystemTasksDBContext> options) 
+            : base(options)
+        { 
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<TaskModel> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

1438aad58a569f91c00b2c8a2b8aaa6d3334d6f8
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-12 10:56:46 -0300

    feat: creating enum task status

---
+++ Enums/StatusTask.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace SistemaDeTarefas.Enums
+{
+    public enum StatusTask
+    {
+        [Description("Task to be done")]
+        TeDone = 0,
+        [Description("Task in progress")]
+        InProgress = 1,
+        [Description("Task completed")]
+        Completed = 2,
+    }
+}

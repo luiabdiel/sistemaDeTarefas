3da41b2b89ea626ae8d7a569d521d8707cc96629
luiabdiel <luiggiabdieldev@gmail.com>
2023-10-12 10:56:26 -0300

    feat: creates model mapping and adds it to the context

--- Data/SystemTasksDBContext.cs
+++ Data/SystemTasksDBContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data.Map;
 using SistemaDeTarefas.Models;
 
 namespace SistemaDeTarefas.Data
@@ -15,6 +16,9 @@ namespace SistemaDeTarefas.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new UserMap());
+            modelBuilder.ApplyConfiguration(new TaskMap());
+
             base.OnModelCreating(modelBuilder);
         }
     }
